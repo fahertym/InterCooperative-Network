f1ed02ddf7daa485139af1908f1bb3489f53ad16
fahertym <matt.faherty@gmail.com>
2024-06-26 00:11:49 -0400

    Update block.py with improved hash calculation and mining function

--- icn/blockchain/block.py
+++ icn/blockchain/block.py
@@ -1,3 +1,5 @@
+# icn/blockchain/block.py
+
 import time
 import json
 import hashlib
@@ -9,26 +11,17 @@ class Block:
         self.timestamp = timestamp
         self.previous_hash = previous_hash
         self.nonce = 0
-        self.hash = None
-        self.calculate_hash()
+        self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        block_dict = {
-            "index": self.index,
-            "transactions": [tx.to_dict() for tx in self.transactions],
-            "timestamp": self.timestamp,
-            "previous_hash": self.previous_hash,
-            "nonce": self.nonce
-        }
-        block_string = json.dumps(block_dict, sort_keys=True)
-        self.hash = hashlib.sha256(block_string.encode()).hexdigest()
-        return self.hash
+        block_string = json.dumps(self.to_dict(), sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
 
     def mine_block(self, difficulty):
         target = "0" * difficulty
         while self.hash[:difficulty] != target:
             self.nonce += 1
-            self.calculate_hash()
+            self.hash = self.calculate_hash()
         print(f"Block mined: {self.hash}")
 
     def to_dict(self):
@@ -37,13 +30,12 @@ class Block:
             "transactions": [tx.to_dict() for tx in self.transactions],
             "timestamp": self.timestamp,
             "previous_hash": self.previous_hash,
-            "nonce": self.nonce,
-            "hash": self.hash
+            "nonce": self.nonce
         }
 
     @classmethod
     def from_dict(cls, block_dict):
-        from ..blockchain.transaction import Transaction  # Import here to avoid circular import
+        from .transaction import Transaction  # Import here to avoid circular import
         block = cls(
             block_dict['index'],
             [Transaction.from_dict(tx) for tx in block_dict['transactions']],
